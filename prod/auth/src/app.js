38aac5d39c7ad21f5bf3bd67fcfaeca668f57792
krittiyaclark <krittiyaclark@gmail.com>
2017-09-03 22:00:04 -0400

    Built log in form with firebase

--- prod/auth/src/app.js
+++ prod/auth/src/app.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyAq-1EmEr4EURLdoyFf05YeYb-9O3agr28',
+        authDomain: 'authentication-b414c.firebaseapp.com',
+        databaseURL: 'https://authentication-b414c.firebaseio.com',
+        projectId: 'authentication-b414c',
+        storageBucket: 'authentication-b414c.appspot.com',
+        messagingSenderId: '179303492945'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
