f6546f8032e9a5f86119d873f191e0a4d9c582a7
krittiyaclark <krittiyaclark@gmail.com>
2017-09-11 23:13:31 -0400

    Created a manager project, setup friebase

---
+++ prod/manager/src/app.js
@@ -0,0 +1,36 @@
+import React, { Component} from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyATJEwfxAT7Z62TawKS1D1H-GxdGvAQ4no",
+      authDomain: "manager-9495d.firebaseapp.com",
+      databaseURL: "https://manager-9495d.firebaseio.com",
+      projectId: "manager-9495d",
+      storageBucket: "manager-9495d.appspot.com",
+      messagingSenderId: "292521456225"
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+
+export default App;
